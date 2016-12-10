c0c6f1ec49edfbcad502b30f1c285dbd4f9f414d
Javier Dorfsman <Javier Dorfsman>
2016-12-10 15:41:07 -0800

    First Commit

---
+++ WindowsToolkit/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsToolkit
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fixPrinterBtn = new System.Windows.Forms.Button();
+            this.fixWifiBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fixPrinterBtn
+            // 
+            this.fixPrinterBtn.Location = new System.Drawing.Point(52, 75);
+            this.fixPrinterBtn.Name = "fixPrinterBtn";
+            this.fixPrinterBtn.Size = new System.Drawing.Size(75, 23);
+            this.fixPrinterBtn.TabIndex = 0;
+            this.fixPrinterBtn.Text = "Fix Printer";
+            this.fixPrinterBtn.UseVisualStyleBackColor = true;
+            this.fixPrinterBtn.Click += new System.EventHandler(this.fixPrinterBtn_Click);
+            // 
+            // fixWifiBtn
+            // 
+            this.fixWifiBtn.Location = new System.Drawing.Point(133, 75);
+            this.fixWifiBtn.Name = "fixWifiBtn";
+            this.fixWifiBtn.Size = new System.Drawing.Size(75, 23);
+            this.fixWifiBtn.TabIndex = 1;
+            this.fixWifiBtn.Text = "Fix WiFi";
+            this.fixWifiBtn.UseVisualStyleBackColor = true;
+            this.fixWifiBtn.Click += new System.EventHandler(this.fixWifiBtn_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 177);
+            this.Controls.Add(this.fixWifiBtn);
+            this.Controls.Add(this.fixPrinterBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Windows Toolkit";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button fixPrinterBtn;
+        private System.Windows.Forms.Button fixWifiBtn;
+    }
+}
+
---
+++ WindowsToolkit/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Diagnostics;
+using System.Management;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsToolkit
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void fixPrinterBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                runProcess("cmd.exe", "/C net stop spooler");
+                runProcess("cmd.exe", "/C net start spooler");
+                printLogMessage("Try now. If it still doesn't work.. try turning off and on the printer and running this tool again. Sorry but that's all I can do for now.");
+            }
+            catch(Exception e1)
+            {
+                printLogMessage("Error running Fix Printer. {0}", e1.Message);
+            }
+        }
+
+        private void runProcess(string command, string arguments)
+        {
+            Process process = new Process();
+
+            // Stop the process from opening a new window
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+
+            // Setup executable and parameters
+            process.StartInfo.FileName = command;
+            process.StartInfo.Arguments = arguments;
+
+            // Go
+            process.Start();
+        }
+
+
+        private void printLogMessage(string msg, params string[] args)
+        {
+            MessageBox.Show(string.Format(msg, args), "Windows Toolkit", MessageBoxButtons.OK);
+        }
+
+        private void fixWifiBtn_Click(object sender, EventArgs e)
+        {
+            bool didItFixedIt = false;
+            //First try to reset the network adapter.
+            didItFixedIt = resetNetworkAdapter();
+            if (didItFixedIt) return;
+
+            if(MessageBox.Show("Restart the computer?", "Windows Toolkit", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                didItFixedIt = restartComputer();
+            }
+
+            if (!didItFixedIt)
+            {
+                printLogMessage("Sorry, but that's all I can do. In the future I'll add more options.");
+            }
+
+        }
+
+        private bool restartComputer()
+        {
+            //Restart the computer
+            try
+            {
+                runProcess("shutdown.exe", "/r /t 0");
+                return true;
+            }
+
+            catch(Exception e1)
+            {
+                printLogMessage("Couldn't restart computer, please restart computer manually. {0}", e1.Message);
+                return false;
+            }
+
+        }
+
+        private bool didItHelpMsgBox()
+        {
+            return MessageBox.Show("Please check if now it works. Does it?", "Did we help?", MessageBoxButtons.YesNo) == DialogResult.Yes;
+        }
+
+        private bool resetNetworkAdapter()
+        {
+            try
+            {
+                bool networkAdapterFound = false;
+                string networkConnectionName = Properties.Settings.Default.NetworkConnectionName;
+                SelectQuery wmiQuery = new SelectQuery("SELECT * FROM Win32_NetworkAdapter WHERE NetConnectionId != NULL");
+                ManagementObjectSearcher searchProcedure = new ManagementObjectSearcher(wmiQuery);
+                foreach (ManagementObject item in searchProcedure.Get())
+                {
+                    if (((string)item["NetConnectionId"]) == networkConnectionName)
+                    {
+                        item.InvokeMethod("Disable", null);
+                        Thread.Sleep(3500);
+                        item.InvokeMethod("Enable", null);
+                        networkAdapterFound = true;
+                    }
+                }
+
+                if (!networkAdapterFound)
+                {
+                    printLogMessage("We coulnd't find any network connection. Please contact support");
+                }
+
+                return didItHelpMsgBox();
+            }
+            catch(Exception e1)
+            {
+                printLogMessage("Error running Reset Network Adapter. {0}", e1.Message);
+                return false;
+            }
+        }
+    }
+}
